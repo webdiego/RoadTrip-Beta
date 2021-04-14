607c790efcdf0649d63a4375de1a33121475bfd7
Your Name <webdiegomassarini@gmail.com>
2021-04-14 18:41:32 +1000

    added animation using framer motin+setTimeOut

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,40 +1,81 @@
-import styled from "styled-components";
-import Home from "./Home";
 import { useState } from "react";
+//Styled Components
+import styled from "styled-components";
+//Framer motion
+import { motion } from "framer-motion";
+//Components
+import Home from "./Home";
+//Icons
 import RoadTripIcon from "../Img/Roadtrip.png";
 import Clouds from "../Img/Clouds.png";
 import OkIcon from "../Img/Ok.png";
 
 //STYLE
-import { Main, RoadTripTitle, RoadTripIconTitle, UserIcon ,AlertContainer,Message, AlertYes } from "../Style/Style";
+import {
+  Main,
+  RoadTripTitle,
+  RoadTripIconTitle,
+  UserIcon,
+  AlertContainer,
+  Message,
+  AlertYes,
+} from "../Style/Style";
+
 function Welcome() {
   const LocalUser = localStorage.getItem("User")! ? localStorage.getItem("User")! : "";
   const [toggle, setToggle] = useState<boolean>(LocalUser.length > 1 ? true : false);
   const [user, setUser] = useState<string>("");
   const [info, setInfo] = useState<boolean>(false);
-
+  const [isOpen, setIsOpen] = useState(false);
   const hideWelcome = () => {
-    localStorage.setItem("User", JSON.stringify(user));
+    setIsOpen(true);
 
-    setToggle(true);
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 1500);
+    setTimeout(() => {
+      localStorage.setItem("User", JSON.stringify(user));
+      setToggle(true);
+    }, 1000);
   };
   const Hide = () => {
-    localStorage.clear();
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 1500);
 
-    setToggle(false);
+    setTimeout(() => {
+      
+      localStorage.clear();
+      setToggle(false);
+    }, 1000);
+ 
   };
- console.log(LocalUser)
- console.log(user)
+
+  const variants = {
+    open: {
+      opacity: 0,
+    },
+    closed: {
+      opacity: 1,
+      transition: { duration: 0.3 },
+    },
+  };
   return (
-    <div style={{position:"relative" , height:"100vh"}}>
+    <motion.div
+      animate={isOpen ? "open" : "closed"}
+      variants={variants}
+      style={{ position: "relative", height: "100vh" }}
+    >
       {info && (
         <AlertContainer>
           <Message>
             <p style={{ margin: "2rem" }}>
-              RoadTrip uses and saves your data on your browser (like your password etc.). <br/> If you clean up your data you will lose your trip.
+              RoadTrip uses and saves your data on your browser (like your password etc.). <br /> If
+              you clean up your data you will lose your trip.
             </p>
             <div>
-              <ButtonAllRight onClick={()=>setInfo(false)}>All right!</ButtonAllRight>
+              <ButtonAllRight onClick={() => setInfo(false)}>All right!</ButtonAllRight>
             </div>
           </Message>
         </AlertContainer>
@@ -42,7 +83,7 @@ function Welcome() {
       {LocalUser.length <= 0 && (
         <div>
           <MainWelcome style={{ backgroundImage: `url(${Clouds})` }}>
-          <ButtonInfo onClick={()=>setInfo(true)}> i </ButtonInfo>
+            <ButtonInfo onClick={() => setInfo(true)}> i </ButtonInfo>
 
             <h4>Welcome to</h4>
             <RoadTripTitle>
@@ -53,19 +94,24 @@ function Welcome() {
 
           <InsertNameContainer>
             <label>Insert Your Name/NickName</label>
-            <div style={{display:"flex" }}>
+            <div style={{ display: "flex" }}>
               <input type="text" value={user} onChange={(e) => setUser(e.target.value)} />
-              <ButtonGo onClick={hideWelcome}> <img src={OkIcon} alt=""/> </ButtonGo>
+              <ButtonGo onClick={hideWelcome}>
+                {" "}
+                <img src={OkIcon} alt="" />{" "}
+              </ButtonGo>
             </div>
           </InsertNameContainer>
         </div>
       )}
 
-      {LocalUser.length > 0 &&  
-      <Home 
-      LocalUser={LocalUser.replace(/['"]+/g, '').trimStart().slice(0,1).toUpperCase()}
-       Hide={Hide} />}
-    </div>
+      {LocalUser.length > 0 && (
+        <Home
+          LocalUser={LocalUser.replace(/['"]+/g, "").trimStart().slice(0, 1).toUpperCase()}
+          Hide={Hide}
+        />
+      )}
+    </motion.div>
   );
 }
 
@@ -93,26 +139,23 @@ const InsertNameContainer = styled.div`
   }
 `;
 const ButtonInfo = styled(UserIcon)`
-  
   font-family: "Fredoka One", cursive;
   cursor: pointer;
-  margin-left:2rem;
+  margin-left: 2rem;
 `;
 const MainWelcome = styled(Main)`
-margin-bottom:0;
-padding: 2rem 0 0 0;
-`
-const ButtonAllRight = styled(AlertYes)`
-
-`
+  margin-bottom: 0;
+  padding: 2rem 0 0 0;
+`;
+const ButtonAllRight = styled(AlertYes)``;
 const ButtonGo = styled(UserIcon)`
-cursor:pointer;
-margin-left:1rem;
-background-color:#129d27;
-border:5px solid #36c110 ;
+  cursor: pointer;
+  margin-left: 1rem;
+  background-color: #129d27;
+  border: 5px solid #36c110;
 
-img{
-  width:2rem;
-  transform:translateY(-2px)
-}
-`
+  img {
+    width: 2rem;
+    transform: translateY(-2px);
+  }
+`;
